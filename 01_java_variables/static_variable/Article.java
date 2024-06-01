4c6e9247bcc40083c3f9f513b89e5e1fcd3507a3
Khushter Adeeb <115522888+Khushteradeeb@users.noreply.github.com>
2024-06-01 17:25:17 +0000

    Static variable completed

--- 01_java_variables/static_variable/Article.java
+++ 01_java_variables/static_variable/Article.java
@@ -1,9 +1,18 @@
 
 class Article 
 {
-
+     
+    // static variable
+    public static double rating;
+    
     public static void main(String[] args) {
-        
+     
+
+        // We don't need to create object ot access the static variable..
+        // we just have to write - class_name.variable_name;
+
+        Article.rating = 4.5;
+        System.out.println("Static Vriable Declaration as rating = " + Article.rating);
     }
 
 }
