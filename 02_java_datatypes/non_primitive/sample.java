cfd6846b02646d6fc9d4556d32626e9b3c2eb535
Khushter Adeeb <115522888+Khushteradeeb@users.noreply.github.com>
2024-06-03 17:31:25 +0000

    basics of  Datatypes completed

---
+++ 02_java_datatypes/non_primitive/sample.java
@@ -0,0 +1,42 @@
+
+
+class sample 
+{
+
+       public static void main(String[] args) 
+       {
+        
+       // Non-primitive Datatypes are String , Array ,Class, interface ect...
+     
+       //  HOW TO CREATE ARRAY
+
+       //  var-name = new type [size];   
+       
+       // Declaring Array
+       //  int intArray[];
+
+        // Allocating memory to array
+        // intArray = new int[20];
+
+        //  Combining the both Statements in one
+        //  int[] intArray = new int[20];
+
+        // DECLARING  ARRAY LITERALS
+
+        //    int[] intArray = new int[]{1,2,3,4,5,6,7,8,9,10};
+
+
+         // HOW TO CREATE STRING
+         // There are two ways to create String 1st is String Literals and 2nd is Using new keyword
+
+         // String Literals
+         //  String demoString = "Khushter Adeeb";
+         
+         // Using new keyword 
+         // String demoString = new String("Khushter Adeeb");
+
+
+
+       }
+
+}
